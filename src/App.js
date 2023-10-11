79220d5458bfadd24a41ab1bf79ac8d0fafb3308
Julita <julia.franchi7@gmail.com>
2023-10-11 17:29:05 -0300

    skills without css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import { Banner } from './Components/Banner/Banner';
 import { NavBar } from './Components/NavBar/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Skills } from './Components/Skills/Skills';
 
 function App() {
 	return (
 		<div className="App">
 			<NavBar />
-
 			<Banner />
+			<Skills />
 		</div>
 	);
 }
---
+++ src/Components/Skills/Skills.jsx
@@ -0,0 +1,93 @@
+import { Col, Container, Row } from 'react-bootstrap';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import jsLogo from '../../assets/javaScripIcon.png';
+import htmlLogo from '../../assets/icons8-html-5-240.png';
+import csslLogo from '../../assets/icons8-css3-300.png';
+import cssTailwindlLogo from '../../assets/icons8-tailwind-css-240.png';
+import reduxlLogo from '../../assets/icons8-redux-240.png';
+import reactlLogo from '../../assets/icons8-reaccionar-nativo-240.png';
+import zustandLogo from '../../assets/zustand.png';
+import BoostrapLogo from '../../assets/icons8-oreja-240.png';
+
+export const Skills = () => {
+	const responsive = {
+		superLargeDesktop: {
+			breakpoint: { max: 4000, min: 3000 },
+			items: 5,
+		},
+		desktop: {
+			breakpoint: { max: 3000, min: 1024 },
+			items: 3,
+		},
+		tablet: {
+			breakpoint: { max: 1024, min: 464 },
+			items: 2,
+		},
+		mobile: {
+			breakpoint: { max: 464, min: 0 },
+			items: 1,
+		},
+	};
+
+	return (
+		<section className="skill" id="skills">
+			<Container>
+				<Row>
+					<Col>
+						<div className="skill-bx">
+							<h2>Tecnologías</h2>
+							<p>
+								{' '}
+								iojqwoiejqwoiejioqwejqowiejioqwejqiowejqwoioiqjweoiqjweoiqwqoije,
+								<br></br> askldjaskldjaslkdj
+							</p>
+							<Carousel
+								responsive={responsive}
+								infinite={true}
+								className="skill-slider"
+							>
+								<div className="item">
+									<img src={jsLogo} alt="Image" />
+									<h5>JavaScript</h5>
+								</div>
+								<div className="item">
+									<img src={htmlLogo} alt="Image" />
+									<h5>HTML</h5>
+								</div>
+								<div className="item">
+									<img src={csslLogo} alt="Image" />
+									<h5>CSS</h5>
+								</div>
+								<div className="item">
+									<img src={cssTailwindlLogo} alt="Image" />
+									<h5>CSS Tailwind</h5>
+								</div>
+								<div className="item">
+									<img src={reduxlLogo} alt="Image" />
+									<h5>Redux</h5>
+								</div>
+								<div className="item">
+									<img src={reactlLogo} alt="Image" />
+									<h5>React</h5>
+								</div>
+								<div className="item">
+									<img
+										src={zustandLogo}
+										alt="Image"
+										style={{ borderRadius: '50%' }}
+									/>
+									<h5>zustand</h5>
+								</div>
+								<div className="item">
+									<img src={BoostrapLogo} alt="Image" />
+									<h5>Boostrap</h5>
+								</div>
+							</Carousel>
+						</div>
+					</Col>
+				</Row>
+			</Container>
+		</section>
+	);
+};
