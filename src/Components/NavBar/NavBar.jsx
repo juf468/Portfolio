7dbbf3a78bf91042c479c4d5f9620b794f8abe35
Julita <julia.franchi7@gmail.com>
2023-10-16 17:11:39 -0300

    links

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -70,10 +70,10 @@ export const NavBar = () => {
 					</Nav>
 					<span className="navbar-text">
 						<div className="social-icon">
-							<a href="#">
+							<a href="https://github.com/juf468">
 								<img src={githubIcon} alt="github" />
 							</a>
-							<a href="#">
+							<a href="https://www.linkedin.com/in/julia-franchi/">
 								<img src={linkdinIcon} alt="linkedin" />
 							</a>
 						</div>
