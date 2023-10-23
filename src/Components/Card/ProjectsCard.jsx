2d1c8a02b1d8d94997816de8e5dbbd1b66f37658
Julita <julia.franchi7@gmail.com>
2023-10-23 19:22:46 -0300

    fixing css

--- src/Components/Card/ProjectsCard.jsx
+++ src/Components/Card/ProjectsCard.jsx
@@ -2,7 +2,7 @@ import { Col } from 'react-bootstrap';
 
 export const ProjectsCard = ({ title, description }) => {
 	return (
-		<Col sm={6} md={4}>
+		<Col sm={12} md={12}>
 			<div className="proj-txtx">
 				<h4>{title}</h4>
 				<span>{description}</span>
